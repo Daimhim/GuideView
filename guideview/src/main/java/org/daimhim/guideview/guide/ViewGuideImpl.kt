af19ceb1b30906fa6c216cece168c2e0b8515492
John <970113287@qq.com>
2020-05-29 15:27:12 +0800

    test

--- guideview/src/main/java/org/daimhim/guideview/guide/ViewGuideImpl.kt
+++ guideview/src/main/java/org/daimhim/guideview/guide/ViewGuideImpl.kt
@@ -1,11 +1,14 @@
 package org.daimhim.guideview.guide
 
 import android.content.Context
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import org.daimhim.guideview.view.MaskView
+
 
 /**
  * @date: 2020/5/25
@@ -13,9 +16,15 @@ import android.view.animation.AnimationUtils
  * @remark: 这个人很懒，什么都没有留下
  */
 class ViewGuideImpl : AbsGuide() {
-    private var onCreateView :View? = null
+    private var onCreateView: View? = null
     override fun show(context: Context, window: Window, overlay: ViewGroup) {
         onCreateView = onCreateView(context, overlay)
+
+        val dm = DisplayMetrics()
+        window.windowManager.defaultDisplay.getMetrics(dm)
+        val params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dm.heightPixels)
+        (onCreateView as MaskView?)?.layoutParams = params
+
         if (onCreateView!!.parent == null) {
             overlay.addView(onCreateView!!)
             if (mConfiguration.mEnterAnimationId != -1) {
@@ -40,7 +49,7 @@ class ViewGuideImpl : AbsGuide() {
     }
 
     override fun dismiss() {
-        val vp = ((onCreateView ?: return).parent ?:return) as ViewGroup
+        val vp = ((onCreateView ?: return).parent ?: return) as ViewGroup
         if (mConfiguration.mExitAnimationId != -1) {
             // mMaskView may leak if context is null
             val context: Context = onCreateView!!.context!!
